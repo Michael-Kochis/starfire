053d31612cc8287e989eec1c5bd56812b0b2e64d
Michael-Kochis <rhishisikk@yahoo.com>
2024-12-02 15:36:57 -0600

    Dec 2024 restart under Java 17.

---
+++ src/main/java/com/kiragames/starfire/StarfireApplication.java
@@ -0,0 +1,13 @@
+package com.kiragames.starfire;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StarfireApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StarfireApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kiragames/starfire/StarfireApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kiragames.starfire;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StarfireApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
